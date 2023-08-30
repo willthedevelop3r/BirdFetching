006ba2c95a0b51a931fbaa2f98896418c0b297c9
William N <williamnguyen773@gmail.com>
2023-08-30 13:13:40 -0500

    initial commit

---
+++ BirdFetching/Controllers/BirdsController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using BirdFetching.Models;
+
+namespace BirdFetching.Controllers
+{   
+    public class BirdsController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiBaseUrl;
+
+        public BirdsController(IConfiguration configuration)
+        {
+            _httpClient = new HttpClient();
+            _apiBaseUrl = configuration["ApiSettings:BaseUrl"];
+        }
+
+        // Get all birds
+        public async Task<IActionResult> AllBirds()
+        {
+            Console.WriteLine("Entered AllBirds action.");
+            string apiUrl = $"{_apiBaseUrl}/birds";
+
+            try
+            {
+                Console.WriteLine("Making HTTP GET request to fetch all birds.");
+                var response = await _httpClient.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+
+                    Console.WriteLine($"Received birds data: {jsonResponse}"); // Log raw JSON response here
+
+                    var birdsData = JsonSerializer.Deserialize<BirdApiResponse>(jsonResponse, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                    Console.WriteLine($"Birds data parsed?: {birdsData}");
+
+                    Console.WriteLine("Successfully parsed birds data.");
+
+                    return View(birdsData?.Data);
+                }
+
+            }
+
+            catch (HttpRequestException ex) 
+            {
+                Console.WriteLine($"Request error: {ex.Message}");
+                return View("Error");
+            }
+
+            return View("Error");
+        }
+        
+        // Get random bird
+        public async Task<IActionResult> RandomBird() 
+        {
+            Console.WriteLine("Entered RandomBird action.");
+            string apiUrl = $"{_apiBaseUrl}/birds/generate";
+
+           try 
+           {
+                Console.WriteLine("Making HTTP GET request to fetch random bird.");
+                var response = await _httpClient.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await response.Content.ReadAsStringAsync();
+
+                    Console.WriteLine($"Received bird data: {jsonResponse}"); // Log raw JSON response here
+
+                    var birdData = JsonSerializer.Deserialize<SingleBirdResponse>(jsonResponse, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                    Console.WriteLine($"Birds data parsed?: {birdData}");
+
+                    Console.WriteLine("Successfully parsed random bird data.");
+
+                    return View(birdData?.Data);
+                }
+           }
+
+           catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"Request error: {ex.Message}");
+                return View("Error");
+            }
+
+            return View("Error");
+        }
+    }
+}
---
+++ BirdFetching/Models/BirdModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace BirdFetching.Models
+{
+    public class BirdModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string? Description { get; set; }
+
+       /* [JsonPropertyName("image_url")]
+        public string? ImageUrl { get; set; }*/
+    }
+
+    public class BirdApiResponse 
+    {
+        [JsonPropertyName("data")]
+        public List<BirdModel> Data { get; set; }
+    }
+
+    public class SingleBirdResponse
+    {
+        [JsonPropertyName("data")]
+        public BirdModel Data { get; set; }
+    }
+}
